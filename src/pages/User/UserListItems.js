d9f6256d5a1f684cf575ea78a16b0caad1e2c713
ruturaj220 <ruturajv.21.bmca@acharya.ac.in>
2023-10-27 18:42:29 +0530

    path setup

--- src/pages/User/UserListItems.js
+++ src/pages/User/UserListItems.js
@@ -8,56 +8,64 @@ import {Link} from "react-router-dom";
 import QuizIcon from '@mui/icons-material/Quiz';
 import Swal from "sweetalert2";
 import HomeIcon from "@mui/icons-material/Home";
-const handleLogout = () => {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You will be logged out of the system.",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, log me out!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // Perform the logout action here
-            if (result.isConfirmed) {
-                window.location.href = '/'; // Redirect to the home page after logout
-            }
-        }
-    });
-};
+import {useLogoutNavigation} from "./auth";
 
-export const userListItems = ({ username }) => (
-    <React.Fragment>
-        <ListItemButton component={Link} to ={`/user/${username}`}>
-            <ListItemIcon>
-                <HomeIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-        </ListItemButton>
-        <ListItemButton component={Link} to={`/user/${username}/profile`}>
-            <ListItemIcon>
-                <AccountCircleIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Profile" />
-        </ListItemButton>
-        <ListItemButton component={Link} to={`/user/${username}/quiz`}>
-            <ListItemIcon>
-                <QuizIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Quiz" />
-        </ListItemButton>
-        {/*<ListItemButton component={Link} to={`/user/${username}/result`}>*/}
-        {/*    <ListItemIcon>*/}
-        {/*        <AddCircleRoundedIcon/>*/}
-        {/*    </ListItemIcon>*/}
-        {/*    <ListItemText primary="Result" />*/}
-        {/*</ListItemButton>*/}
-        <ListItemButton onClick={handleLogout}>
-            <ListItemIcon>
-                <LogoutIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Log Out" />
-        </ListItemButton>
-    </React.Fragment>
-);
+// const handleLogout = () => {
+//     Swal.fire({
+//         title: 'Are you sure?',
+//         text: "You will be logged out of the system.",
+//         icon: 'warning',
+//         showCancelButton: true,
+//         confirmButtonColor: '#3085d6',
+//         cancelButtonColor: '#d33',
+//         confirmButtonText: 'Yes, log me out!'
+//     }).then((result) => {
+//         if (result.isConfirmed) {
+//             // Perform the logout action here
+//             if (result.isConfirmed) {
+//                 window.location.href = '/'; // Redirect to the home page after logout
+//             }
+//         }
+//     });
+// };
+
+export const userListItems = ({ username }) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { handleLogout } = useLogoutNavigation();
+
+    return(
+        <React.Fragment>
+            <ListItemButton component={Link} to ={`/user/${username}`}>
+                <ListItemIcon>
+                    <HomeIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+            </ListItemButton>
+            <ListItemButton component={Link} to={`/user/${username}/profile`}>
+                <ListItemIcon>
+                    <AccountCircleIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Profile" />
+            </ListItemButton>
+            <ListItemButton component={Link} to={`/user/${username}/quiz`}>
+                <ListItemIcon>
+                    <QuizIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Quiz" />
+            </ListItemButton>
+            {/*<ListItemButton component={Link} to={`/user/${username}/result`}>*/}
+            {/*    <ListItemIcon>*/}
+            {/*        <AddCircleRoundedIcon/>*/}
+            {/*    </ListItemIcon>*/}
+            {/*    <ListItemText primary="Result" />*/}
+            {/*</ListItemButton>*/}
+            <ListItemButton onClick={handleLogout}>
+                <ListItemIcon>
+                    <LogoutIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Log Out" />
+            </ListItemButton>
+        </React.Fragment>
+    );
+
+}
