c8f2bda1c6a29bd7443f5396f22e58cb4045ff27
ruturaj220 <ruturajvaskar220@gmail.com>
2024-04-01 19:00:50 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,16 +9,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-=======
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
->>>>>>> ec424c48beb9050062d0efaa721c116605bd3350
